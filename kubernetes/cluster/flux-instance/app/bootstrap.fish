6ab436bb7c5b3b6a31178ba30176d3ff7bddaaad
Joseph Petersen <me@jetersen.dev>
2026-01-21 19:58:34 +0100

    wait for flux-operator before installing flux-instance

--- kubernetes/cluster/flux-instance/app/bootstrap.fish
+++ kubernetes/cluster/flux-instance/app/bootstrap.fish
@@ -8,6 +8,10 @@ set SOURCE $DIR/source.yaml
 set VERSION (yq '.spec.ref.tag' $SOURCE)
 set CHART (yq '.spec.url' $SOURCE)
 
+# Wait for flux-operator to be ready
+echo "Waiting for flux-operator to be ready..."
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=flux-operator -n flux-system --timeout=300s
+
 # Extract values from HelmRelease and install
 yq -o yaml '.spec.values' $HELMRELEASE | helm install flux-instance $CHART \
     --namespace flux-system \
